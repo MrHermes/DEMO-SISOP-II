fdfa6c4febf29a169a5943b4630e7e555af617ef
Frederick Hidayat <90272678+MrHermes@users.noreply.github.com>
2023-04-05 21:10:46 +0700

    Add files via upload

---
+++ demo.c
@@ -0,0 +1,58 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <wait.h>
+
+int main(int argc, char** argv){
+    pid_t child_id = fork();
+    int status;
+
+    if (child_id < 0){
+            exit(EXIT_FAILURE);
+    }
+    if (child_id == 0){
+        execlp("bin/bash", "bash", "-c", "ps | grep daemon",  NULL);
+    }
+    else {
+        wait(&status);
+        execlp("mkdir", "mkdir", "output" ,NULL);
+    }
+
+    //testing
+    child_id=fork();
+
+    if (child_id < 0){
+            exit(EXIT_FAILURE);
+    }
+    if (child_id == 0){
+        execlp("touch", "touch", "output/total-daemon-process.txt" ,NULL);
+    }
+
+    pid_t cid = fork();
+    if (cid < 0){
+        exit(EXIT_FAILURE);
+    }
+    if (cid == 0){
+        execlp("bin/chmod", "chmod", "+x", "total-daemon-process.txt", NULL);
+        exit(0);
+    }
+
+    cid = fork();
+    if (cid < 0){
+        exit(EXIT_FAILURE);
+    }
+    if (cid == 0){
+        execlp("rm", "rm", "ouput/total-daemon-process.txt", NULL);
+        exit(0);
+    }
+
+}
